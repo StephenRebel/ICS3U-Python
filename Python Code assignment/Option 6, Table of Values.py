7e51f11b34ed9817f9a29cc420b4f8ac69bc7093
StephenRebel <sabr.rebel@gmail.com>
2020-12-11 11:11:32 -0500

    Added graphing for functions, and fixed some bugs.

--- Python Code assignment/Option 6, Table of Values.py
+++ Python Code assignment/Option 6, Table of Values.py
@@ -4,6 +4,9 @@ import time
 # Imports only the system and name from the os library
 from os import system, name
 
+# Importing a grpahing library
+import matplotlib.pyplot as plt 
+
 # Function that will determin the type of os the user is running
 # and will clear the sceen based on the os found
 def clear():
@@ -13,14 +16,23 @@ def clear():
         _ = system('clear')
 
 # A function that will get user input for the variables and return them
-def user_var():
+def user_var(func_type):
     # Bullet proofing to assure valid numbers are input
     while True:
         try:
             a = float(input("Please enter the value for a: "))
             b = float(input("Please enter the value for b: "))
             c = float(input("Please enter the value for c: "))
-            break
+            if func_type == 1:
+                if b == 0:
+                    print("For a linear function b cannot = 0")
+                else:
+                    break
+            elif func_type == 2:
+                if a == 0:
+                    print("For a quadratic function a cannot = 0")
+                else:
+                    break
         # Handles errors that would crash the program
         except ValueError:
             clear()
@@ -63,8 +75,35 @@ def adv_user_var(adv_func_type):
         print(" {:17.1f} {:31.2f} ".format(equa_x, y))
         equa_x += 1
 
+    # Resets the x value for graphing calculations
+    equa_x = h - 4
+    # stores values for a graph in two lists
+    while equa_x <= h + 4:
+        if adv_func_type == 1:
+            y_list.append(round(a*(equa_x - h)**3 + k, 3))
+        elif adv_func_type == 2:
+            y_list.append(round(a*abs(equa_x - h) + k, 3))
+        else:  
+            y_list.append(round(a*b**(equa_x - h) + k, 3))
+        x_list.append(round(equa_x, 2))
+        equa_x += 0.1
+    # Creates the graph
+    plt.plot(x_list, y_list)
+    plt.xlabel('X - axis')
+    plt.ylabel('Y - axis')
+    if adv_func_type == 1:
+        plt.title(f"Graph of the cubic function y = {a}(x - {h})^3 + {k}")
+    elif adv_func_type == 2:
+        plt.title(f"Graph of the absolute function y = {a}|x - {h}| + {k}")
+    else:
+        plt.title(f"Graph of the exponential function y = {a}x^(x - {h}) + {k}")
+    plt.show()
+
 # Beggining of main program loop
 while True:
+    # Initializing and resetting the graph value lists
+    x_list = []
+    y_list = []
     # Bullet proofing for user input of which type of function they would like
     while True:
         try:
@@ -83,24 +122,45 @@ while True:
     if func_type == 1:
         clear()
         print("For an equation in the form: 0 = ax + by + c")
-        equa_vars = user_var()
+        equa_vars = user_var(1)
         for x in range(-4, 5):
             y = -(equa_vars[0]*x + equa_vars[2]) / equa_vars[1]
             if y == 0:
                 y = 0
             print(" {:17.1f} {:31.2f} ".format(x, y))
+            x_list.append(round(x, 2))
+            y_list.append(round(y, 3))
+        # Creating line graph using previous values
+        plt.plot(x_list, y_list)
+        plt.xlabel('X - axis') 
+        plt.ylabel('Y - axis')
+        plt.title(f"Graph of the linear function y = -({equa_vars[0]}x + {equa_vars[2]}) / {equa_vars[1]}") 
+        plt.show()
 
     # Prints the table of values for a quadratic function using . format
     elif func_type == 2:
         clear()
         print("For an equation in the form: y = ax^2 + bx + c")
-        equa_vars = user_var()
+        equa_vars = user_var(2)
         quad_vertex = -equa_vars[1]/(2*equa_vars[0])
         equa_x = quad_vertex - 4
         while equa_x <= quad_vertex + 4:
             y = equa_vars[0]*equa_x**2 + equa_vars[1]*equa_x + equa_vars[2]
             print(" {:17.1f} {:31.2f} ".format(equa_x, y))
             equa_x += 1
+        # restes x value fro graph
+        equa_x = quad_vertex - 4 
+        while equa_x <= quad_vertex + 4:
+            # create lists for graph values
+            y_list.append(round(equa_vars[0]*equa_x**2 + equa_vars[1]*equa_x + equa_vars[2], 3))
+            x_list.append(round(equa_x, 2))
+            equa_x += 0.1
+        # Opens the graph
+        plt.plot(x_list, y_list)
+        plt.xlabel('X - axis') 
+        plt.ylabel('Y - axis')
+        plt.title(f"Graph of the quadratic function y = {equa_vars[0]}x + {equa_vars[1]}x + {equa_vars[2]}") 
+        plt.show()
 
     # gets user input for the 3 extra functions
     else:
